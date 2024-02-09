b9abd811a4610f5a0878f40d21cd080a8952ff03
Kyle <kcparks1234@gmail.com>
2024-02-09 15:30:20 -0800

    Changed file structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,13 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import React from 'react';
+import Navbar from "@/components/navbar";
+import HomeLayout from "./layout";
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <div className="flex justify-center items-center p-12">
-      <Tabs defaultValue="account" className="w-[300px] flex flex-col">
-        <TabsList>
-          <TabsTrigger value="home">Home</TabsTrigger>
-          <TabsTrigger value="about">About</TabsTrigger>
-          <TabsTrigger value="projects">Projects</TabsTrigger>
-          <TabsTrigger value="contact">Contact</TabsTrigger>
-        </TabsList>
-        <TabsContent value="home" className="flex-grow flex justify-center items-center">
-          {/* Separate container for the h1 text */}
-          <div className="text-center p-28">
-            <h1 className="text-8xl whitespace-nowrap">
-              Kyle Parks
-            </h1>
-            <p className="text-4xl text-center p-6">
-              Full Stack Developer
-            </p>
-          </div>
-        </TabsContent>
-        <TabsContent value="about">Change your password here.</TabsContent>
-      </Tabs>
-    </div>
+    <HomeLayout>
+      <Navbar />
+    </HomeLayout>
   );
 }
+
+export default HomePage;
---
+++ components/navbar.tsx
@@ -0,0 +1,28 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+export default function Navbar() {
+  return (
+    <div className="flex justify-center items-center p-12">
+      <Tabs defaultValue="home" className="w-[300px] flex flex-col">
+        <TabsList>
+          <TabsTrigger value="home">Home</TabsTrigger>
+          <TabsTrigger value="about">About</TabsTrigger>
+          <TabsTrigger value="projects">Projects</TabsTrigger>
+          <TabsTrigger value="contact">Contact</TabsTrigger>
+        </TabsList>
+        <TabsContent value="home" className="flex-grow flex justify-center items-center">
+          {/* Separate container for the h1 text */}
+          <div className="text-center p-28">
+            <h1 className="text-8xl whitespace-nowrap">
+              Kyle Parks
+            </h1>
+            <p className="text-4xl text-center p-6">
+              Full Stack Developer
+            </p>
+          </div>
+        </TabsContent>
+        <TabsContent value="about">Change your password here.</TabsContent>
+      </Tabs>
+    </div>
+  );
+}
