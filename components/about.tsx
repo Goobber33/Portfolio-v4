a2145c1ab422821c115073e8425675501c0d298b
Kyle <kcparks1234@gmail.com>
2024-02-11 01:46:19 -0800

    Added inView to a new paragraph centered below the previous content in my about section.

--- components/about.tsx
+++ components/about.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { motion } from 'framer-motion';
+import { InView } from 'react-intersection-observer'; // Ensure you've imported InView
 
 const AboutPage = () => {
 
@@ -10,6 +11,20 @@ const AboutPage = () => {
       opacity: 1,
       transition: {
         staggerChildren: 0.3,
+      },
+    },
+  };
+
+  // Corrected to use scrollVariants
+  const scrollVariants = {
+    offscreen: { y: 20, opacity: 0 },
+    onscreen: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 1,
       },
     },
   };
@@ -26,23 +41,34 @@ const AboutPage = () => {
   };
 
   return (
-    <><motion.div variants={containerVariants}
-      initial="hidden"
-      animate="visible" className='flex flex-col lg:flex-row gap-8 sm:gap-12 md:gap-16 lg:gap-24 p-20 lg:p-40 items-center'>
-      <motion.img
-        variants={childVariants}
-        src="/kyle.jpeg"
-        alt="Kyle Parks"
-        className="rounded-full w-48 sm:w-64 md:w-72 lg:w-96" />
-      <motion.div variants={childVariants} className='text-white font-extrabold text-2xl text-center lg:max-w-4xl w-96'>
-        With a focus on user-centric design<span style={{ color: '#49c5b6' }}>,</span> I&apos;ve dedicated myself to crafting engaging and accessible digital solutions. From concept to deployment<span style={{ color: '#49c5b6' }}>,</span> I enjoy every step of the creative process.
+    <>
+      <motion.div variants={containerVariants}
+        initial="hidden"
+        animate="visible" className='flex flex-col lg:flex-row gap-8 sm:gap-12 md:gap-16 lg:gap-24 p-20 lg:p-40 items-center'>
+        <motion.img
+          variants={childVariants}
+          src="/kyle.jpeg"
+          alt="Kyle Parks"
+          className="rounded-full w-48 sm:w-64 md:w-72 lg:w-96" />
+        <motion.h3 variants={childVariants} className='text-white font-extrabold text-md sm:text-2xl md:text-3xl lg:text-2xl text-center lg:max-w-4xl w-96'>
+          With a focus on user-centric design<span style={{ color: '#49c5b6' }}>,</span> I&apos;ve dedicated myself to crafting engaging and accessible digital solutions. From concept to deployment<span style={{ color: '#49c5b6' }}>,</span> I enjoy every step of the creative process.
+        </motion.h3>
       </motion.div>
-    </motion.div> 
-    <motion.div>
 
-      </motion.div></>
+      <InView as="div" threshold={0.2} triggerOnce={true}>
+        {({ inView, ref }) => (
+          <div ref={ref} className="items-center mt-10">
+            <motion.p className="text-white font-bold text-2xl text-center" initial="offscreen" animate={inView ? "onscreen" : "offscreen"} variants={scrollVariants}>
+              I was born and raised in San Diego California, and currently, I live in Coeur d&apos;Alene Idaho.
+            </motion.p>
+            <motion.p className="text-white font-bold text-2xl text-center" initial="offscreen" animate={inView ? "onscreen" : "offscreen"} variants={scrollVariants}>
+              I am a Full Stack Web Developer with a focus on user-centric design.
+            </motion.p>
+          </div>
+        )}
+      </InView>
+    </>
   );
 }
 
 export default AboutPage;
-
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -35,10 +35,10 @@ export default function Navbar() {
         <div className="md:flex flex-col justify-center items-center px-4 py-8 sm:p-12 w-full">
             <Tabs value={currentTab} onValueChange={handleTabChange}>
                 <TabsList className="text-sm sm:text-md flex justify-center whitespace-nowrap">
-                    <TabsTrigger value="home" className="m-1 px-2 py-1 sm:px-4 sm:py-2 text-base sm:text-lg">Home</TabsTrigger>
-                    <TabsTrigger value="about" className="m-1 px-2 py-1 sm:px-4 sm:py-2 text-base sm:text-lg">About</TabsTrigger>
-                    <TabsTrigger value="projects" className="m-1 px-2 py-1 sm:px-4 sm:py-2 text-base sm:text-lg">Projects</TabsTrigger>
-                    <TabsTrigger value="contact" className="m-1 px-2 py-1 sm:px-4 sm:py-2 text-base sm:text-lg">Contact</TabsTrigger>
+                    <TabsTrigger value="home" className="m-1 px-2 py-1 sm:px-4 sm:py-2 text-base sm:text-lg font-bold">Home</TabsTrigger>
+                    <TabsTrigger value="about" className="m-1 px-2 py-1 sm:px-4 sm:py-2 text-base sm:text-lg font-bold">About</TabsTrigger>
+                    <TabsTrigger value="projects" className="m-1 px-2 py-1 sm:px-4 sm:py-2 text-base sm:text-lg font-bold">Projects</TabsTrigger>
+                    <TabsTrigger value="contact" className="m-1 px-2 py-1 sm:px-4 sm:py-2 text-base sm:text-lg font-bold">Contact</TabsTrigger>
                 </TabsList>
                 <TabsContent value="home" className="flex-grow flex justify-center items-center">
                     <Home />
