16530c32510ff1659355ee4f44cfeb68a3d513c0
Kyle <kcparks1234@gmail.com>
2024-02-11 00:57:11 -0800

    Added changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+"use client"
+
+import React, { useState, useEffect } from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Home from "@/components/home";
 import About from "@/components/about";
 
 export default function Navbar() {
-    return (
- 
-            <div className="md:flex flex-col justify-center items-center px-4 py-8 sm:p-12 w-full">
-                <Tabs defaultValue="home">
+  // Initialize state with the value from local storage or default to 'home'
+  const [currentTab, setCurrentTab] = useState(() => localStorage.getItem('currentTab') || 'home');
+
+  // Update local storage when the currentTab changes
+  useEffect(() => {
+    localStorage.setItem('currentTab', currentTab);
+  }, [currentTab]);
+
+  const handleTabChange = (newValue: string) => {
+    setCurrentTab(newValue);
+  };
+
+  return (
+    <div className="md:flex flex-col justify-center items-center px-4 py-8 sm:p-12 w-full">
+                <Tabs value={currentTab} onValueChange={handleTabChange}>
                     <TabsList className="text-sm sm:text-md flex justify-center whitespace-nowrap">
                         <TabsTrigger value="home" className="m-1 px-2 py-1 sm:px-4 sm:py-2 text-base sm:text-lg">Home</TabsTrigger>
                         <TabsTrigger value="about" className="m-1 px-2 py-1 sm:px-4 sm:py-2 text-base sm:text-lg">About</TabsTrigger>
@@ -24,3 +37,4 @@ export default function Navbar() {
             </div>
     );
 }
+
