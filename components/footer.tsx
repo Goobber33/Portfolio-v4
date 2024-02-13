6a69dec740e0df6700993dcf0c0287f64f82a350
Kyle <kcparks1234@gmail.com>
2024-02-13 00:52:57 -0800

    Added my tech icons and my final paragraph to my about section.

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import { SocialIcon } from 'react-social-icons';
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center p-8">
+    <footer className="flex justify-center items-center p-12">
       <div className="flex space-x-4">
         <SocialIcon url="https://github.com/Goobber33" target="_blank" fgColor="#fff" className="transition-transform duration-200 hover:scale-110" style={{ height: 50, width: 50 }} />
         <SocialIcon url="https://www.linkedin.com/in/kyle-parks-b0a74017b" target="_blank" fgColor="#fff" className="transition-transform duration-200 hover:scale-110" style={{ height: 50, width: 50 }} />
